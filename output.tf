93661b39ba4b6d2c848721ba7b7f8f5224c3e794
akbar <akbaralihussain@gmail.com>
2024-02-10 21:42:45 -0800

    removed provider ref

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "cdn_url" {
   value = (local.origin_access == "public") ? aws_cloudfront_distribution.public[0].domain_name : (local.origin_access == "oac") ? aws_cloudfront_distribution.oac[0].domain_name : aws_cloudfront_distribution.oai[0].domain_name
+  description = "URL of CDN created for the Static Website"
 }
 
 output "website_url" {
   value = "https://${var.domain_name}"
+  description = "URL of the Static Website"
 }
